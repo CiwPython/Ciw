96d9cadc278bc5e9f64c8c0f705b1a61bd6c0603
Lieke <lieke-52@hotmail.com>
2015-10-29 13:04:33 +0000

    add previous class to individual

--- qnetsim/tests/test_individual.py
+++ qnetsim/tests/test_individual.py
@@ -6,6 +6,7 @@ class TestIndividual(unittest.TestCase):
     def test_init_method_1(self):
         i = qnetsim.Individual(22, 3)
         self.assertEqual(i.customer_class, 3)
+        self.assertEqual(i.previous_class, 3)
         self.assertEqual(i.id_number, 22)
         self.assertEqual(i.service_start_date, False)
         self.assertEqual(i.service_time, False)
@@ -16,6 +17,7 @@ class TestIndividual(unittest.TestCase):
     def test_init_method_2(self):
         i = qnetsim.Individual(5)
         self.assertEqual(i.customer_class, 0)
+        self.assertEqual(i.previous_class, 0)
         self.assertEqual(i.id_number, 5)
         self.assertEqual(i.service_start_date, False)
         self.assertEqual(i.service_time, False)
