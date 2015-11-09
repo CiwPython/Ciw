6d152f447598023b518d83592833314aad3087c7
Lieke <lieke-52@hotmail.com>
2015-11-09 11:49:23 +0000

    do not need zero in the masterschedule

--- qnetsim/node.py
+++ qnetsim/node.py
@@ -58,7 +58,7 @@ class Node:
             >>> N.c
             1
             >>> N.masterschedule
-            [0, 30, 60, 90, 100, 130, 160, 190, 200, 230, 260, 290]
+            [30, 60, 90, 100, 130, 160, 190, 200, 230, 260, 290]
 
         """
 
@@ -69,7 +69,7 @@ class Node:
             self.schedule = self.simulation.parameters[self.simulation.c[id_number-1]]
             self.cyclelength = self.simulation.parameters['cycle_length']
             self.c = self.schedule[0][1]
-            self.masterschedule = [i*self.cyclelength + obs for i in range(self.simulation.max_simulation_time//self.cyclelength + 1) for obs in [t[0] for t in  self.schedule]]
+            self.masterschedule = [i*self.cyclelength + obs for i in range(self.simulation.max_simulation_time//self.cyclelength + 1) for obs in [t[0] for t in  self.schedule]][1:]
         else:
             self.c = self.simulation.c[id_number-1]
 
