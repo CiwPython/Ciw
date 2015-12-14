eff5213a0ec1ed6f5374d6e2f7c64c264a81d33c
Vince Knight <vincent.knight@gmail.com>
2015-12-14 15:39:44 +0000

    Cleaning the setup.py script.
    The actual package is needed.

--- setup.py
+++ setup.py
@@ -5,13 +5,7 @@ setup(
     version='0.0.1dev',
     author='Geraint Palmer, Vincent Knight',
     author_email=('palmer.geraint@googlemail.com'),
-    #scripts=['simulation.py, analyse.py, experiment.py, run_simulation.py'],
+    packages = ['asq'],
+    scripts = ['run_simulation'],
     description='A discrete event simulation framework for open queueing networks',
-    # install_requires=[
-    #     "matplotlib >= 1.4.2",
-    #     "PyYAML==3.11",
-    #     "networkx==1.9.1",
-    #     "numpy==1.9.2",
-    #     "scipy==0.15.1",
-    # ],
 )
