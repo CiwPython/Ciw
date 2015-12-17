be8c66c05d69684212909dabfe81daf6946a6a76
geraint <palmer.geraint@googlemail.com>
2015-12-17 15:47:20 +0000

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     packages = ['ciw'],
     scripts = ['run_simulation'],
     description='A discrete event simulation framework for open queueing networks',
+    install_requires=["PyYAML==3.11", "networkx==1.9.1", "docopt==0.6.2"]
 )
