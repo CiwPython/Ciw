41c9b19c9ea871bc6de8e58c012bb3d3ec54ee46
Geraint Palmer <palmer.geraint@googlemail.com>
2016-01-06 16:21:05 +0000

    removing version numbers from requirements, update version number to 0.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name='Ciw',
-    version='0.0.1',
+    version='0.0.2',
     author='Geraint Palmer, Vincent Knight',
     author_email=('palmer.geraint@googlemail.com'),
     packages = ['ciw'],
     scripts = ['run_simulation'],
     description='A discrete event simulation framework for open queueing networks',
-    install_requires=["PyYAML==3.11", "networkx==1.9.1", "docopt==0.6.2",
-                      "hypothesis==1.17"]
+    install_requires=["PyYAML", "networkx", "docopt", "hypothesis"]
 )
