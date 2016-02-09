27b689cff41d2028ac4704da3fa2c02dcf10112e
Geraint Palmer <palmer.geraint@googlemail.com>
2016-02-09 00:37:13 +0000

    changes.txt and setup.py ready for release v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Ciw',
-    version='0.0.2',
+    version='0.0.3',
     author='Geraint Palmer, Vincent Knight',
     author_email=('palmer.geraint@googlemail.com'),
     packages = ['ciw'],
