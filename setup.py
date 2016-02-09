4a6b394e9bd23f45fbcd5f0922a7489108bca6e8
Geraint Palmer <palmer.geraint@googlemail.com>
2016-02-09 00:18:53 +0000

    remove runsimulation script from setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author='Geraint Palmer, Vincent Knight',
     author_email=('palmer.geraint@googlemail.com'),
     packages = ['ciw'],
-    scripts = ['run_simulation'],
     description='A discrete event simulation framework for open queueing networks',
     install_requires=["PyYAML", "networkx", "docopt", "hypothesis"]
 )
