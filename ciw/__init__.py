67323816891e385f7d7c5f8ead5afe91f7d6c385
Geraint Palmer <palmer.geraint@googlemail.com>
2016-04-04 10:28:11 +0100

    ready for v0.0.6

--- ciw/__init__.py
+++ ciw/__init__.py
@@ -6,6 +6,7 @@ from arrival_node import ArrivalNode
 from exit_node import ExitNode
 from node import Node
 from state_tracker import *
+from exactnode import *
 from import_params import load_parameters
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
