eabe4ae433bb42d63d239bf78bb9565ef2a3d8f2
Geraint Palmer <palmer.geraint@googlemail.com>
2016-04-18 16:03:10 +0100

    tests for servicecentre object, tests fail

---
+++ ciw/tests/test_network.py
@@ -0,0 +1,39 @@
+import unittest
+import ciw
+from hypothesis import given
+from hypothesis.strategies import floats, integers, lists, random_module
+
+
+class TestServiceCentre(unittest.TestCase):
+
+    def test_init_method(self):
+    	number_of_servers = 2
+    	queueing_capacity = 'Inf'
+    	class_change_matrix = [[0.2, 0.8],
+    	                       [1.0, 0.0]]
+    	schedule = None
+        SC = ciw.ServiceCentre(number_of_servers,
+        	                   queueing_capacity,
+        	                   class_change_matrix,
+        	                   schedule)
+        self.assertEqual(SC.number_of_servers, number_of_servers)
+        self.assertEqual(SC.queueing_capacity, queueing_capacity)
+        self.assertEqual(SC.class_change_matrix, class_change_matrix)
+        self.assertEqual(SC.schedule, schedule)
+
+    @given(number_of_servers=integers(min_value=1),
+           queueing_capacity=integers(min_value=0),
+           class_change_prob1=floats(min_value=0.0, max_value=1.0),
+           class_change_prob2=floats(min_value=0.0, max_value=1.0))
+    def test_init_method_h(self, number_of_servers, queueing_capacity, class_change_prob1, class_change_prob2):
+    	class_change_matrix = [[class_change_prob1, 1 - class_change_prob1],
+    	                       [class_change_prob2, 1 - class_change_prob2]]
+    	schedule = None
+        SC = ciw.ServiceCentre(number_of_servers,
+        	                   queueing_capacity,
+        	                   class_change_matrix,
+        	                   schedule)
+        self.assertEqual(SC.number_of_servers, number_of_servers)
+        self.assertEqual(SC.queueing_capacity, queueing_capacity)
+        self.assertEqual(SC.class_change_matrix, class_change_matrix)
+        self.assertEqual(SC.schedule, schedule)
