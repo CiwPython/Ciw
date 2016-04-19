40f8aa8bf9853677311b0d4c6da46a7209b84ff3
Geraint Palmer <palmer.geraint@googlemail.com>
2016-04-19 14:03:25 +0100

    tidyup

--- ciw/import_params.py
+++ ciw/import_params.py
@@ -24,46 +24,46 @@ def Network_From_Dictionary(params):
             for cls in xrange(len(params['Arrival_distributions']))]
     if isinstance(params['Arrival_distributions'], list):
         arrivals = [params['Arrival_distributions']]
-
     if isinstance(params['Service_distributions'], dict):
         services = [params['Service_distributions']['Class ' + str(cls)]
             for cls in xrange(len(params['Service_distributions']))]
     if isinstance(params['Service_distributions'], list):
         services = [params['Service_distributions']]
-
     if isinstance(params['Transition_matrices'], dict):
         transitions = [params['Transition_matrices']['Class ' + str(cls)]
             for cls in xrange(len(params['Transition_matrices']))]
     if isinstance(params['Transition_matrices'], list):
         transitions = [params['Transition_matrices']]
-
     number_of_classes = params.get('Number_of_classes', len(arrivals))
     number_of_nodes = params.get('Number_of_nodes', len(arrivals[0]))
     queueing_capacities = params.get('Queue_capacities',
         ['Inf' for i in xrange(number_of_nodes)])
     class_change_matrices = params.get('Class_change_matrices',
         {'Node ' + str(nd + 1): None for nd in xrange(number_of_nodes)})
-    number_of_servers = []
-    schedules = []
+    number_of_servers, schedules, nodes, classes = [], [], [], []
     for c in params['Number_of_servers']:
         if isinstance(c, str):
             number_of_servers.append('schedule')
             schedules.append(params[c])
         else:
-
             number_of_servers.append(c)
-            schedules.append(None)
-    
-    nodes = []
+            schedules.append(None)    
     for nd in xrange(number_of_nodes):
-        nodes.append(ServiceCentre(number_of_servers[nd],
-                                  queueing_capacities[nd],
-                                  class_change_matrices['Node ' + str(nd + 1)],
-                                  schedules[nd]))
-    classes = []
+        nodes.append(ServiceCentre(
+            number_of_servers[nd],
+            queueing_capacities[nd],
+            class_change_matrices['Node ' + str(nd + 1)],
+            schedules[nd]))
     for cls in xrange(number_of_classes):
-        classes.append(CustomerClass(arrivals[cls],
-                                     services[cls],
-                                     transitions[cls]))
-    N = Network(nodes, classes)
-    return N
+        classes.append(CustomerClass(
+            arrivals[cls],
+            services[cls],
+            transitions[cls]))
+    return Network(nodes, classes)
+
+def Network_From_File(directory_name):
+    """
+    Creates a Network object form a yaml file
+    """
+    params = load_parameters(directory_name)
+    return Network_From_Dictionary(params)
--- ciw/tests/test_network.py
+++ ciw/tests/test_network.py
@@ -12,10 +12,7 @@ class TestServiceCentre(unittest.TestCase):
         class_change_matrix = [[0.2, 0.8],
                                [1.0, 0.0]]
         schedule = None
-        SC = ciw.ServiceCentre(number_of_servers,
-                               queueing_capacity,
-                               class_change_matrix,
-                               schedule)
+        SC = ciw.ServiceCentre(number_of_servers, queueing_capacity, class_change_matrix, schedule)
         self.assertEqual(SC.number_of_servers, number_of_servers)
         self.assertEqual(SC.queueing_capacity, queueing_capacity)
         self.assertEqual(SC.class_change_matrix, class_change_matrix)
@@ -26,13 +23,12 @@ class TestServiceCentre(unittest.TestCase):
            class_change_prob1=floats(min_value=0.0, max_value=1.0),
            class_change_prob2=floats(min_value=0.0, max_value=1.0))
     def test_init_method_h(self, number_of_servers, queueing_capacity, class_change_prob1, class_change_prob2):
-        class_change_matrix = [[class_change_prob1, 1 - class_change_prob1],
-                               [class_change_prob2, 1 - class_change_prob2]]
+        class_change_matrix = [[class_change_prob1,
+                                1 - class_change_prob1],
+                               [class_change_prob2,
+                                1 - class_change_prob2]]
         schedule = None
-        SC = ciw.ServiceCentre(number_of_servers,
-                               queueing_capacity,
-                               class_change_matrix,
-                               schedule)
+        SC = ciw.ServiceCentre(number_of_servers, queueing_capacity, class_change_matrix, schedule)
         self.assertEqual(SC.number_of_servers, number_of_servers)
         self.assertEqual(SC.queueing_capacity, queueing_capacity)
         self.assertEqual(SC.class_change_matrix, class_change_matrix)
@@ -50,9 +46,7 @@ class TestCustomerClass(unittest.TestCase):
                                  ["Exponential", 5]]
         transition_matrix = [[.2, .6, .2], [0, 0, 0], [.5, 0, 0]]
 
-        CC = ciw.CustomerClass(arrival_distributions,
-                               service_distributions,
-                               transition_matrix)
+        CC = ciw.CustomerClass(arrival_distributions, service_distributions, transition_matrix)
         self.assertEqual(CC.arrival_distributions, arrival_distributions)
         self.assertEqual(CC.service_distributions, service_distributions)
         self.assertEqual(CC.transition_matrix, transition_matrix)
@@ -62,32 +56,23 @@ class TestCustomerClass(unittest.TestCase):
 class TestNetwork(unittest.TestCase):
 
     def test_init_method(self):
-        service_centres = []
-        for i in xrange(4):
-            number_of_servers = 2
-            queueing_capacity = 'Inf'
-            class_change_matrix = [[0.2, 0.8],
-                                   [1.0, 0.0]]
-            schedule = None
-            service_centres.append(ciw.ServiceCentre(number_of_servers,
-                                           queueing_capacity,
-                                           class_change_matrix,
-                                           schedule))
-        customer_classes = []
-        for i in xrange(2):
-            arrival_distributions = [["Uniform", 4.0, 9.0],
-                                     ["Exponential", 5],
-                                     ["Gamma", 0.6, 1.2]]
-            service_distributions = [["Gamma", 4.0, 9.0],
-                                     ["Uniform", 0.6, 1.2],
-                                     ["Exponential", 5]]
-            transition_matrix = [[.2, .6, .2], [0, 0, 0], [.5, 0, 0]]
-            customer_classes.append(ciw.CustomerClass(arrival_distributions,
-                                             service_distributions,
-                                             transition_matrix))
-
+        number_of_servers = 2
+        queueing_capacity = 'Inf'
+        schedule = None
+        class_change_matrix = [[0.2, 0.8],
+                               [1.0, 0.0]]
+        arrival_distributions = [["Uniform", 4.0, 9.0],
+                                 ["Exponential", 5.0],
+                                 ["Gamma", 0.6, 1.2]]
+        service_distributions = [["Gamma", 4.0, 9.0],
+                                 ["Uniform", 0.6, 1.2],
+                                 ["Exponential", 5]]
+        transition_matrix = [[0.2, 0.6, 0.2],
+                             [0.0, 0.0, 0.0],
+                             [0.5, 0.0, 0.0]]
+        service_centres = [ciw.ServiceCentre(number_of_servers, queueing_capacity, class_change_matrix, schedule) for i in xrange(4)]
+        customer_classes = [ciw.CustomerClass(arrival_distributions, service_distributions, transition_matrix) for i in xrange(2)]
         N = ciw.Network(service_centres, customer_classes)
-
         self.assertEqual(N.service_centres, service_centres)
         self.assertEqual(N.customer_classes, customer_classes)
         self.assertEqual(N.number_of_service_centres, 4)
@@ -115,14 +100,17 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(N.customer_classes[0].transition_matrix, [[0.5]])
 
 
-        params = {'Arrival_distributions': [['Exponential', 3.0], ['Uniform', 0.2, 0.6]],
-                  'Service_distributions': [['Exponential', 7.0], ['Deterministic', 0.7]],
+        params = {'Arrival_distributions': [['Exponential', 3.0],
+                                            ['Uniform', 0.2, 0.6]],
+                  'Service_distributions': [['Exponential', 7.0],
+                                            ['Deterministic', 0.7]],
                   'Number_of_servers': ['my_amazing_schedule', 3],
-                  'Transition_matrices': [[0.5, 0.2], [0.0, 0.0]],
+                  'Transition_matrices': [[0.5, 0.2],
+                                          [0.0, 0.0]],
                   'Queue_capacities': [10, 'Inf'],
-                  'my_amazing_schedule': [[20, 1], [50, 4]]}
+                  'my_amazing_schedule': [[20, 1],
+                                          [50, 4]]}
         N = ciw.Network_From_Dictionary(params)
-        
         self.assertEqual(N.number_of_service_centres, 2)
         self.assertEqual(N.number_of_customer_classes, 1)
         self.assertEqual(N.service_centres[0].queueing_capacity, 10)
@@ -147,9 +135,9 @@ class TestNetwork(unittest.TestCase):
                                           'Class 1': [[0.0]]},
                   'Number_of_nodes': 1,
                   'Queue_capacities': ['Inf'],
-                  'Class_change_matrices': {'Node 1': [[0.0, 1.0], [0.2, 0.8]]}}
+                  'Class_change_matrices': {'Node 1': [[0.0, 1.0],
+                                                       [0.2, 0.8]]}}
         N = ciw.Network_From_Dictionary(params)
-        
         self.assertEqual(N.number_of_service_centres, 1)
         self.assertEqual(N.number_of_customer_classes, 2)
         self.assertEqual(N.service_centres[0].queueing_capacity, 'Inf')
@@ -163,3 +151,34 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(N.customer_classes[1].service_distributions, [['Uniform', 0.4, 1.2]])
         self.assertEqual(N.customer_classes[1].transition_matrix, [[0.0]])
 
+
+    def test_network_from_file(self):
+        N = ciw.Network_From_File(
+          'ciw/tests/datafortesting/logs_test_for_simulation/parameters.yml')
+        self.assertEqual(N.number_of_service_centres, 4)
+        self.assertEqual(N.number_of_customer_classes, 3)
+        self.assertEqual(N.service_centres[0].queueing_capacity, 20)
+        self.assertEqual(N.service_centres[1].queueing_capacity, 'Inf')
+        self.assertEqual(N.service_centres[2].queueing_capacity, 30)
+        self.assertEqual(N.service_centres[3].queueing_capacity, 'Inf')
+        self.assertEqual(N.service_centres[0].number_of_servers, 9)
+        self.assertEqual(N.service_centres[1].number_of_servers, 10)
+        self.assertEqual(N.service_centres[2].number_of_servers, 8)
+        self.assertEqual(N.service_centres[3].number_of_servers, 8)
+        self.assertEqual(N.service_centres[0].class_change_matrix, None)
+        self.assertEqual(N.service_centres[1].class_change_matrix, None)
+        self.assertEqual(N.service_centres[2].class_change_matrix, None)
+        self.assertEqual(N.service_centres[3].class_change_matrix, None)
+        self.assertEqual(N.service_centres[0].schedule, None)
+        self.assertEqual(N.service_centres[1].schedule, None)
+        self.assertEqual(N.service_centres[2].schedule, None)
+        self.assertEqual(N.service_centres[3].schedule, None)
+        self.assertEqual(N.customer_classes[0].arrival_distributions, [['Exponential', 3.0], ['Exponential', 7.0], ['Exponential', 4.0], ['Exponential', 1.0]])
+        self.assertEqual(N.customer_classes[1].arrival_distributions, [['Exponential', 2.0], ['Exponential', 3.0], ['Exponential', 6.0], ['Exponential', 4.0]])
+        self.assertEqual(N.customer_classes[2].arrival_distributions, [['Exponential', 2.0], ['Exponential', 1.0], ['Exponential', 2.0], ['Exponential', 0.5]])
+        self.assertEqual(N.customer_classes[0].service_distributions, [['Exponential', 7.0], ['Exponential', 7.0], ['Gamma', 0.4, 0.6], ['Deterministic', 0.5]])
+        self.assertEqual(N.customer_classes[1].service_distributions, [['Exponential', 7.0], ['Triangular', 0.1, 0.85, 0.8], ['Exponential', 8.0], ['Exponential', 5.0]])
+        self.assertEqual(N.customer_classes[2].service_distributions, [['Deterministic', 0.3], ['Deterministic', 0.2], ['Exponential', 8.0], ['Exponential', 9.0]])
+        self.assertEqual(N.customer_classes[0].transition_matrix, [[0.1, 0.2, 0.1, 0.4], [0.2, 0.2, 0.0, 0.1], [0.0, 0.8, 0.1, 0.1], [0.4, 0.1, 0.1, 0.0]])
+        self.assertEqual(N.customer_classes[1].transition_matrix, [[0.6, 0.0, 0.0, 0.2], [0.1, 0.1, 0.2, 0.2], [0.9, 0.0, 0.0, 0.0], [0.2, 0.1, 0.1, 0.1]])
+        self.assertEqual(N.customer_classes[2].transition_matrix, [[0.0, 0.0, 0.4, 0.3], [0.1, 0.1, 0.1, 0.1], [0.1, 0.3, 0.2, 0.2], [0.0, 0.0, 0.0, 0.3]])
