16774665f8a1597a5eddb8d632aeeb1cfe322265
Geraint Palmer <palmer.geraint@googlemail.com>
2018-05-04 18:24:35 +0100

    Ciw docs 2 sided pdf

--- docs/conf.py
+++ docs/conf.py
@@ -228,6 +228,8 @@ latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
+'classoptions': ',openany,oneside',
+
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
 
